4745289646700acc2d0b72a78e9a03525aa53c8f
stefanidisgeorge <geo_stef@ieee.org>
2015-11-16 02:46:12 +0200

    Revert "Update dbconnect.php"
    This reverts commit 176ffa4f1aac4741e2e93411617687a78ebc48c3.

--- dbconnect.php
+++ dbconnect.php
@@ -2,9 +2,9 @@
 error_reporting(-1);
 
 define('DB_HOST', 'vasilis.pw');
-define('DB_NAME', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_NAME', 'admin_watt');
+define('DB_USERNAME', 'admin_watt');
+define('DB_PASSWORD', 'uowm45');
 try {
 $odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
 } 
