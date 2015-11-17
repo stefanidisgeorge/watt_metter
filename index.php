cbfbba9fbc31520d0f3e40ac7ab53aeca08051fa
Alexandros Modis <modis.alex@gmail.com>
2015-11-17 09:45:19 +0200

    Added include('menu.php');

--- index.php
+++ index.php
@@ -19,28 +19,8 @@
 
 <body>
     <section class="container">
-		 <nav class="navbar navbar-inverse" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex8-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Watt Meter</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex8-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Link</a></li>
-			<li><a href="#">Link</a></li>
-
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </nav>
+    	<!--Take navbar from a seperate php file-->
+	<?php include('menu.php');  ?>
         <div class="jumbotron" style="text-align: center">
             <h1>Watt Meter</h1>
 			
